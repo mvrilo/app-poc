4323380a8a7f47afd110dcd5e5c2ae29f6841dc7
Murilo Santana <mvrilo@gmail.com>
2020-05-27 18:13:07 -0300

    validation rules via anonymous struct in the service implementation

--- core/store/service.go
+++ core/store/service.go
@@ -3,6 +3,7 @@ package store
 import (
 	"context"
 
+	"github.com/mvrilo/storepoc/pkg/validator"
 	"github.com/mvrilo/storepoc/proto/v1"
 )
 
@@ -11,10 +12,32 @@ type Service struct {
 }
 
 func (s *Service) Find(ctx context.Context, in *proto.FindRequest) (*proto.Store, error) {
+	data := struct {
+		Id string `validate:"required"`
+	}{
+		in.Id,
+	}
+
+	if err := validator.ValidateGrpc(data); err != nil {
+		return nil, err
+	}
+
 	return s.repo.Find(in)
 }
 
 func (s *Service) Create(ctx context.Context, in *proto.CreateRequest) (*proto.Store, error) {
+	data := struct {
+		Name string `validate:"required"`
+		Uri  string `validate:"required,uri"`
+	}{
+		in.Name,
+		in.Uri,
+	}
+
+	if err := validator.ValidateGrpc(data); err != nil {
+		return nil, err
+	}
+
 	return s.repo.Create(in)
 }
 
@@ -23,5 +46,17 @@ func (s *Service) List(ctx context.Context, in *proto.ListRequest) (*proto.Store
 }
 
 func (s *Service) ChangeStatus(ctx context.Context, in *proto.ChangeStatusRequest) (*proto.Store, error) {
+	data := struct {
+		Id     string       `validate:"required,uuid"`
+		Status proto.Status `validate:"required"`
+	}{
+		in.Id,
+		in.Status,
+	}
+
+	if err := validator.ValidateGrpc(data); err != nil {
+		return nil, err
+	}
+
 	return s.repo.ChangeStatus(in)
 }
